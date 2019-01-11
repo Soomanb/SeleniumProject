399f7f82e2aa84efd7df8e6498cc5ac5ac26982c
ibm <ibm@9.84.225.129>
2019-01-11 10:41:01 +0530

    All Weeks (Simple, Medium, Complex) TC - Commit

---
+++ final-framework-testng/documents/UsersData.sql
@@ -0,0 +1,5 @@
+create table userdata(
+username varchar(50) not null,orderid varchar(50) not null,customer varchar(50) not null,firstname varchar(50) not null,lastname varchar(50) not null,email varchar(50) not null,phone varchar(50) not null,product varchar(50) not null,model varchar(50) not null);
+
+
+insert into userdata values("90", "sooman", "sooman", "bose", "sbose@abc.com", "9876543210", "Integer vitae iaculis massa", "SKU-003"); 
